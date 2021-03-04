ca41a899b85967f486d0b5949a1adf35caa82718
Abhishek Kumar <abhishek.kumar@swanbay.tv>
2021-03-04 12:59:34 +0530

    fix

--- assets/pre-loader/preloader-initi.js
+++ assets/pre-loader/preloader-initi.js
@@ -1,4 +1,4 @@
-
+var path="/var/www/html"
 		$(window).on('load', function() { // makes sure the whole site is loaded 
 			$('#status').fadeOut(); // will first fade out the loading animation 
             $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
